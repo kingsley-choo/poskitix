6b11ec4b21bab9742c14cfcc46f95fe36473e279
jsamaze <111722374+jsamaze@users.noreply.github.com>
2024-03-19 20:52:28 +0800

    complex 1

--- join_queue/join_queue.py
+++ join_queue/join_queue.py
@@ -1,18 +1,58 @@
 from os import environ
 from flask_cors import CORS
+from flask import Flask, request, jsonify
+
 import requests
+import pika
+import json
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/queue/event/<int:eid>/join", methods=["POST"])
-def join_queue(eid):
-    #step 4 and 5
-    r = request.get(f"http://queue:5004/event/{eid}")
-    if r.status_code == 404:
+#activity log and error log missing
+@app.route("/queue/event/<int:eid>/user/<int:uid>/join", methods=["POST"])
+def join_queue(eid,uid):
+    #step 4 and 5 - does the event exist? 
+    r_event = requests.get(f"http://localhost:5002/event/{eid}")
+    if r_event.status_code // 200 != 1:
+        return r_event.text, 404
+    #step 6 and 7 - is there still tickets in the event?
+    r = requests.get(f"http://localhost:5003/ticket/event/{eid}")
+    if r.status_code //200 != 1:
         return r.text, 404
+    #step 10 and 11 - does this user exist?
+    r_user = requests.get(f"http://localhost:5001/user/{uid}")
+    if r_user.status_code // 100 != 2:
+        return r_user.text, r_user.status_code
+    #step 8 and 9 - please let this user join
+    r = requests.post(f"http://localhost:5004/queue", json= {
+        "eid" : eid,
+        "uid" :uid
+    })  
+    if r.status_code // 100 != 2:
+
+        return r.text, r.status_code
+    #step 12
+    exchange="email"
+    body = {
+        "user" : r_user.json()["data"],
+        "event" : r_event.json()["data"]
+    }
+    channel.exchange_declare(exchange=exchange,
+                         exchange_type='topic', durable=True)
+    channel.basic_publish(exchange=exchange,
+                      routing_key='waiting',
+                      body=json.dumps(body))
+    return {
+        "status" : 200,
+        "data" : "success join queue"
+    }
+
     
-    
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5100, debug=True)
