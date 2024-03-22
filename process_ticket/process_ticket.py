bce0b30e333e0cd92b0f614797e320cc153d51d9
zanzzxc <gxlee.2022@scis.smu.edu.sg>
2024-03-22 18:55:22 +0800

    updated process_ticket.py
    sajoifwe

--- process_ticket/process_ticket.py
+++ process_ticket/process_ticket.py
@@ -12,9 +12,28 @@ channel = connection.channel()
 app = Flask(__name__)
 CORS(app)
 
+def email(r, event, s):
+    for person in r.json()["updated_entries"]:
+                uid = person["uid"]
+                r_user = requests.get(f"http://localhost:5001/user/{uid}")
+                #step 10 & 11 getting emails of all the users with status changed
+                if r_user.status_code //200 != 1:
+                    return r_user.text, 404
+                    #step 12
+                exchange="email"
+                body = {
+                    "user" : r_user.json()["data"],
+                    "event" : event
+                }
+                channel.exchange_declare(exchange=exchange,
+                                    exchange_type='topic', durable=True)
+                channel.basic_publish(exchange=exchange,
+                                routing_key= s,
+                                body=json.dumps(body))
+
 #activity log and error log missing
 @app.route("/queue/event/<int:eid>/user/<int:uid>/process")
-def process_ticket(eid,uid):
+def process_ticket(eid):
     #step 2 and 3 - get all future events
     r_event = requests.get(f"http://localhost:5002/event")
     if r_event.status_code // 200 != 1:
@@ -34,55 +53,57 @@ def process_ticket(eid,uid):
             if r_waitingtofail.status_code // 100 != 2:
                 return r_waitingtofail.text, r_waitingtofail.status_code
             #step 9 - returned ID of impacted users in "updated_entries"
-            for person in r_waitingtofail.json()["updated_entries"]:
-                uid = person["uid"]
-                #step 10 & 11 getting emails of all the users with status changed
-                r_user = requests.get(f"http://localhost:5001/user/{uid}")
-                if r_user.status_code //200 != 1:
-                    return r_user.text, 404
-                #step 12
-                exchange="email"
-                body = {
-                    "user" : r_user.json()["data"],
-                    "event" : event.json()["data"]
-                }
-                channel.exchange_declare(exchange=exchange,
-                                    exchange_type='topic', durable=True)
-                channel.basic_publish(exchange=exchange,
-                                routing_key='waiting.fail',
-                                body=json.dumps(body))
-                return {
-                    "status" : 200,
-                    "data" : "Sorry, the tickets for this event is sold out."
-                }
+            email(r_waitingtofail, event, "waiting.fail")
+            # for person in r_waitingtofail.json()["updated_entries"]:
+            #     uid = person["uid"]
+            #     #step 10 & 11 getting emails of all the users with status changed
+            #     r_user = requests.get(f"http://localhost:5001/user/{uid}")
+            #     if r_user.status_code //200 != 1:
+            #         return r_user.text, 404
+            #     #step 12
+            #     exchange="email"
+            #     body = {
+            #         "user" : r_user.json()["data"],
+            #         "event" : event
+            #     }
+            #     channel.exchange_declare(exchange=exchange,
+            #                         exchange_type='topic', durable=True)
+            #     channel.basic_publish(exchange=exchange,
+            #                     routing_key='waiting.fail',
+            #                     body=json.dumps(body))
+        
         #CASE A - there are still tickets in this event
         else:
             #step 7a & 8 changing status
+            r_readytomissed = requests.get(f"http://localhost:5004/queue/event/{eid}/ready-missed")
+            if r_readytomissed.status_code // 100 != 2:
+                return r_readytomissed.text, r_readytomissed.status_code
+            email(r_readytomissed, event, "ready.missed")
+
             r_waitingtoready = requests.get(f"http://localhost:5004/queue/event/{eid}/waiting-ready/")
             if r_waitingtoready.status_code // 100 != 2:
                 return r_waitingtoready.text, r_waitingtoready.status_code
             #step 9 - returned ID of impacted users in "updated_entries"
-            for person in r_waitingtoready.json()["updated_entries"]:
-                uid = person["uid"]
-                r_user = requests.get(f"http://localhost:5001/user/{uid}")
-                #step 10 & 11 getting emails of all the users with status changed
-                if r_user.status_code //200 != 1:
-                    return r_user.text, 404
-                    #step 12
-                exchange="email"
-                body = {
-                    "user" : r_user.json()["data"],
-                    "event" : event.json()["data"]
-                }
-                channel.exchange_declare(exchange=exchange,
-                                    exchange_type='topic', durable=True)
-                channel.basic_publish(exchange=exchange,
-                                routing_key='waiting.ready',
-                                body=json.dumps(body))
-                return {
-                    "status" : 200,
-                    "data" : "You have 15 minutes to buy the ticket from now. Hurry!"
-                }
+            email(r_waitingtoready, event, "waiting.ready")
+            # for person in r_waitingtoready.json()["updated_entries"]:
+            #     uid = person["uid"]
+            #     r_user = requests.get(f"http://localhost:5001/user/{uid}")
+            #     #step 10 & 11 getting emails of all the users with status changed
+            #     if r_user.status_code //200 != 1:
+            #         return r_user.text, 404
+            #         #step 12
+            #     exchange="email"
+            #     body = {
+            #         "user" : r_user.json()["data"],
+            #         "event" : event
+            #     }
+            #     channel.exchange_declare(exchange=exchange,
+            #                         exchange_type='topic', durable=True)
+            #     channel.basic_publish(exchange=exchange,
+            #                     routing_key='waiting.ready',
+            #                     body=json.dumps(body))
+    return {
+            "status" : 200,}
 
 
 if __name__ == "__main__":
