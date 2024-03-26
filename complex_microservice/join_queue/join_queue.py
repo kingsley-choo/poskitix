bef28bc4ddd546bc7e652dec7f1398bd282f6fd3
jsamaze <111722374+jsamaze@users.noreply.github.com>
2024-03-27 06:22:25 +0800

    some help....

--- complex_microservice/join_queue/join_queue.py
+++ complex_microservice/join_queue/join_queue.py
@@ -15,47 +15,85 @@ CORS(app)
 #joshie's function, not sure what to put for the route tho
 @app.route("/event/user/<int:uid>", methods=["GET"])
 def get_events_to_display(uid):
-    r_events = request.get(f"http://localhost:5002/event")["data"]
 
-    for event in r_events:
+    #2. get a response        #1. make a request....
+    response_events = request.get(f"http://localhost:5002/event")
+
+    #did it fail? lets check
+    if response_events.status_code not in range(200,300):
+        return response_events.text,response_events.status_code # REFLECT the result - STOP because cannot proceed without events
+
+    #if it reaches here then the success must have passed
+    events_json_body = response_events.json()
+    #lets get the events inside
+    events = events_json_body["data"]
+
+    # this is how one event looks like
+    # {
+    #     "eid" : ....,
+    #     "event name" : ...,
+    # }
+    # we want to ADD information i.e. whether user status in queue and ticket remaining
+
+    for event in events:
         input_eid = event["eid"]
-        r_tickets_left = request.get(f"http://localhost:5003/ticket/event/{input_eid}")["data"]["tickets"]
 
-        if r_tickets_left == 0:
-            event["status"] = "Sold Out"
-            return event["status"]
+        #FIRST, how many tickets are left?
+        response_tickets_left = request.get(f"http://localhost:5003/ticket/event/{input_eid}")
 
+        if response_events.status_code not in range(200,300): # if there is an error with tickets of one event - leave it be 
+            event["status"] = "error"
+
+        response_tickets_left_body = response_tickets_left.json()
+
+        #oh no more, so we can conclude they probably not in queue
+        if response_tickets_left_body["data"]["tickets_left"] == 0:
+            event["sold_out"] = True
+            event["queue_status"] = None
+        #lets check their status in queue
         else:
-            #paiseh not sure whats the output so i put "status" correct if im wrong
-            response = request.get(f"http://localhost:5004/queue/event/{input_eid}/user/{uid}")
-            response_body = response.json()
-            return response_body
+            event["sold_out"]=False
+            #lets check their status in queue
+            response_queue_status = request.get(f"http://localhost:5004/queue/event/{input_eid}/user/{uid}")
+            if response_queue_status.status_code not in range(200,300):
+                #an error! lets just set the queue_status as None
+                event["queue_status"] = None
+            else:
+                event["queue_status"] = response_queue_status.json()["data"]
+    
+    return { "code" : 200, "data" : events}
 
 
-["data"]["status"]
-#activity log and error log missing
 @app.route("/queue/event/<int:eid>/user/<int:uid>/join", methods=["POST"])
 def join_queue(eid,uid):
+
+
     #step 4 and 5 - does the event exist? 
     r_event = requests.get(f"http://localhost:5002/event/{eid}")
     if r_event.status_code // 200 != 1:
         return r_event.text, 404
+    
+
     #step 6 and 7 - is there still tickets in the event?
     r = requests.get(f"http://localhost:5003/ticket/event/{eid}")
     if r.status_code //200 != 1:
         return r.text, 404
+    
+
     #step 10 and 11 - does this user exist?
     r_user = requests.get(f"http://localhost:5001/user/{uid}")
     if r_user.status_code // 100 != 2:
         return r_user.text, r_user.status_code
+    
+
     #step 8 and 9 - please let this user join
     r = requests.post(f"http://localhost:5004/queue", json= {
         "eid" : eid,
         "uid" :uid
     })  
     if r.status_code // 100 != 2:
-
         return r.text, r.status_code
+    
     #step 12
     exchange="email"
     body = {
@@ -69,7 +107,7 @@ def join_queue(eid,uid):
                       body=json.dumps(body))
     return {
         "status" : 200,
-        "data" : "success join queue"
+        "data" : "success"
     }
 
 if __name__ == "__main__":
