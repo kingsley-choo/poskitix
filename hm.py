b5cf95f21f68d3a4ebb0ce3fe586771594770af9
jsamaze <111722374+jsamaze@users.noreply.github.com>
2024-03-28 06:17:22 +0800

    use complex microservice

--- hm.py
+++ hm.py
@@ -3,10 +3,7 @@ people_to_join = list(range(15))
 
 while people_to_join:
     user_id = random.choice(people_to_join)
-    r= requests.post("http://localhost:5004/queue",json= {
-        "eid": 1,
-        "uid": user_id+1
-    })
+    r= requests.post(f"http://localhost:5100/queue/event/1/user/{user_id+1}/join")
     print(f"added user {user_id} to queue")
     time.sleep(1)
 
