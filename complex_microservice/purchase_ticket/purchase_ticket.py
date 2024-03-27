b2102c61b3f33b6913fa4f347e079d20b803713d
zanzzxc <gxlee.2022@scis.smu.edu.sg>
2024-03-27 22:22:43 +0800

    jfioifehioq
    isgwheoighwe

--- complex_microservice/purchase_ticket/purchase_ticket.py
+++ complex_microservice/purchase_ticket/purchase_ticket.py
@@ -20,47 +20,22 @@ db = SQLAlchemy(app)
 
 CORS(app)
 
-
-class User(db.Model):
-    __tablename__ = "user"
-
-    uid = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(255), nullable=False)
-
-    def __init__(self, uid, username, email):
-        self.uid = uid
-        self.username = username
-        self.email = email
-
-    def json(self):
+@app.route("/purchase_ticket/event/<int:eid>/user/<int:uid>")
+def purchase_ticket(eid,uid):
+    #is the user ready to buy ticket? -> Check user status if it is ready
+    r = requests.get(f"http://localhost:5004/queue/{eid}/user/{uid}")
+    status = r.json()['data']['status']
+    if status == 'Ready':
+        #invoke payment here
+        #pass is supposed to return URL for checkout
+        print('payment')
+    else:
         return {
-            "uid": self.uid,
-            "username": self.username,
-            "email": self.email,
-        }
-
-@app.route("/user/<string:email>")
-def find_by_email(email):
-    output_uid = db.session.scalars(db.select(User).filter_by(email=email).limit(1)).first()
-
-    if output_uid:
-        return jsonify({"code": 200, "data": output_uid.json()})
-    return jsonify({"code": 404, "message": "User not found."}), 404
-
-@app.route("/queue/payment/<int:eid>/user/<int:uid>")
-def purchase_ticket(eid, uid):
-    r_status = request.get(f"http://localhost:5004/{eid}/user/{uid}")
+            "code" : 400,
+            "message" : "Failed to create payment url."
+        }, 400
 
 
-#activity log and error log missing
-@app.route("/order/success", methods=["GET"])
-def process_payment():
-    #step 2 and 3 - get all future payments
-    r_payment = requests.get(f"http://localhost:4242/success.html")
-    if r_payment.status_code // 200 != 1:
-        return r_payment.text, 404
-    return {"status" : 200}
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5300, debug=True)
