4b46ba11ed33ba756f9deb30d6bd3682b284a439
jsamaze <111722374+jsamaze@users.noreply.github.com>
2024-03-27 23:59:20 +0800

    clean up

--- complex_microservice/process_ticket/process_ticket.py
+++ complex_microservice/process_ticket/process_ticket.py
@@ -27,31 +27,15 @@ def process_ticket():
             return r_ticket.text, 404
         #step 6 get number of tickets for this event
         tickets_left = r_ticket.json()["data"]["tickets_left"]
+        print(tickets_left)
         #CASE B - no tickets in this event
         if tickets_left == 0:
             #step 7b & 8 changing status
-            r_waitingtofail = requests.get(f"http://localhost:5004/queue/event/{eid}/waiting-fail")
+            r_waitingtofail = requests.put(f"http://localhost:5004/queue/event/{eid}/waiting-fail")
+            print(r_waitingtofail.text)
             if r_waitingtofail.status_code // 100 == 2:
             #step 9 - returned ID of impacted users in "updated_entries"
                 email(r_waitingtofail, event, "waiting.fail")
-            # for person in r_waitingtofail.json()["updated_entries"]:
-            #     uid = person["uid"]
-            #     #step 10 & 11 getting emails of all the users with status changed
-            #     r_user = requests.get(f"http://localhost:5001/user/{uid}")
-            #     if r_user.status_code //200 != 1:
-            #         return r_user.text, 404
-            #     #step 12
-            #     exchange="email"
-            #     body = {
-            #         "user" : r_user.json()["data"],
-            #         "event" : event
-            #     }
-            #     channel.exchange_declare(exchange=exchange,
-            #                         exchange_type='topic', durable=True)
-            #     channel.basic_publish(exchange=exchange,
-            #                     routing_key='waiting.fail',
-            #                     body=json.dumps(body))
-        
         #CASE A - there are still tickets in this event
         else:
             #step 7a & 8 changing status
@@ -59,7 +43,10 @@ def process_ticket():
             if r_readytomissed.status_code // 100 == 2:
                 email(r_readytomissed, event, "ready.missed")
 
-            r_waitingtoready = requests.put(f"http://localhost:5004/queue/event/{eid}/waiting-ready/")
+            r_waitingtoready = requests.put(f"http://localhost:5004/queue/event/{eid}/waiting-ready", json={
+                "tickets_remaining":tickets_left
+            })
+            print(r_waitingtoready.text)
             if r_waitingtoready.status_code // 100 == 2:
             #step 9 - returned ID of impacted users in "updated_entries"
                 email(r_waitingtoready, event, "waiting.ready")
@@ -85,24 +72,23 @@ def process_ticket():
 
 def email(r, event, s):
     for person in r.json()["updated_entries"]:
-                uid = person["uid"]
-                r_user = requests.get(f"http://localhost:5001/user/{uid}")
-                #step 10 & 11 getting emails of all the users with status changed
-                if r_user.status_code //200 != 1:
-                    return r_user.text, 404
-                    #step 12
-                exchange="email"
-                body = {
-                    "user" : r_user.json()["data"],
-                    "event" : event
-                }
-                channel.exchange_declare(exchange=exchange,
-                                    exchange_type='topic', durable=True)
-                channel.basic_publish(exchange=exchange,
-                                routing_key= s,
-                                body=json.dumps(body))
-
-print(app.url_map)
+        uid = person["uid"]
+        r_user = requests.get(f"http://localhost:5001/user/{uid}")
+        #step 10 & 11 getting emails of all the users with status changed
+        if r_user.status_code //200 != 1:
+            continue
+            #step 12
+        exchange="email"
+        body = {
+            "user" : r_user.json(),
+            "event" : event
+        }
+        print(body)
+        channel.exchange_declare(exchange=exchange,
+                            exchange_type='topic', durable=True)
+        channel.basic_publish(exchange=exchange,
+                        routing_key= s,
+                        body=json.dumps(body))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5200, debug=True)
